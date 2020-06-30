90d91581e9fb5e39f19d9d2a9aedf108a2bb55a3
Rahul Gopinath <rahul@gopinath.org>
2020-06-30 07:18:44 -0700

    use the right paths

--- klee/mjs/run_exp.sh
+++ klee/mjs/run_exp.sh
@@ -2,4 +2,4 @@ RUNTIME=$1
 SUBJECT=mjs
 rm -rf klee-*
 clang -c -g -emit-llvm ${SUBJECT}.c -o ${SUBJECT}.bc -ldl
-time timeout -k2 ${RUNTIME}h klee --optimize --only-output-states-covering-new --libc=uclibc  -posix-runtime ${SUBJECT}.bc -sym-stdin 200 > log.txt 2>&1 &
+time timeout -k2 ${RUNTIME}h klee --optimize --only-output-states-covering-new --libc=uclibc  -posix-runtime ${SUBJECT}.bc -sym-stdin 200 > log.txt 2>&1
--- pfuzzer/samples/csv/run_exp.sh
+++ pfuzzer/samples/csv/run_exp.sh
@@ -2,10 +2,10 @@ RUNTIME=$1
 SUBJECT=$2
 CURRENTDIR=$(pwd)
 sh clean.sh
-../../install/bin/trace-instr $(pwd)/${SUBJECT}.c ../excluded_functions > log.txt 2>&1
+$HOME/taints/install/bin/trace-instr $(pwd)/${SUBJECT}.c ../excluded_functions > log.txt 2>&1
 cd ../../chains
-time timeout -k9 ${RUNTIME}h python3 chains.py -p ${CURRENTDIR}/${SUBJECT}.c -a False -i False 1> /dev/null 2> error.log &
-/bin/sleep ${RUNTIME}h
-/bin/sleep 15m
+time timeout -k9 ${RUNTIME}h python3 chains.py -p ${CURRENTDIR}/${SUBJECT}.c -a False -i False 1> /dev/null 2> error.log
+#/bin/sleep ${RUNTIME}h
+#/bin/sleep 15m
 cd $CURRENTDIR
 sh get_cov.sh ${SUBJECT} ${CURRENTDIR}
