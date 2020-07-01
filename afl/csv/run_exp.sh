5da4964fa55b8fe98a28985c3387bbeba04cf5b3
Rahul Gopinath <rahul@gopinath.org>
2020-07-01 05:06:20 -0700

    set AFL_NO_AFFINITY=True to let multiple instances run together

--- afl/csv/run_exp.sh
+++ afl/csv/run_exp.sh
@@ -1,5 +1,5 @@
 rm -rf findings/
 $HOME/chains/src/programs/afl-2.52b/afl-gcc csvparser.c -o csv
 mkdir -p findings/queue findings/hangs
-export AFL_SKIP_CPUFREQ=True
+export AFL_SKIP_CPUFREQ=True AFL_NO_AFFINITY=True
 time timeout -k9 ${1}h ../../programs/afl-2.52b/afl-fuzz -i tests -o findings ./csv > log.txt 2>&1
--- afl/mjs/run_exp.sh
+++ afl/mjs/run_exp.sh
@@ -1,5 +1,5 @@
 rm -rf findings/
 $HOME/chains/src/programs/afl-2.52b/afl-gcc mjs.c -o mjs -ldl
 mkdir -p findings/queue findings/hangs
-export AFL_SKIP_CPUFREQ=True
+export AFL_SKIP_CPUFREQ=True AFL_NO_AFFINITY=True
 time timeout -k9 ${1}h ../../programs/afl-2.52b/afl-fuzz -i tests -o findings ./mjs > log.txt 2>&1
