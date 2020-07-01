8bf5ca3e83c01cff8ba50867392ada4e84c6779a
Rahul Gopinath <rahul@gopinath.org>
2020-07-01 03:54:11 -0700

    make sure evaluation happens after run

--- klee/run_all.sh
+++ klee/run_all.sh
@@ -1,21 +1,12 @@
 RUNTIME=$1
 : ${RUNTIME:=0.01}
 
-(cd csv; sh run_exp.sh $RUNTIME) &
-(cd ini; sh run_exp.sh $RUNTIME) &
-(cd cjson; sh run_exp.sh $RUNTIME) &
-(cd tinyc; sh run_exp.sh $RUNTIME) &
-(cd mjs; sh run_exp.sh $RUNTIME) &
+( (cd csv; sh run_exp.sh $RUNTIME) ; (cd csv/eval/; sh comp_run.sh csvparser) ) &
+( (cd ini; sh run_exp.sh $RUNTIME) ; (cd ini/eval/; sh comp_run.sh ini) ) &
+( (cd cjson; sh run_exp.sh $RUNTIME) ; (cd cjson/eval/; sh comp_run.sh cJSON) ) &
+( (cd tinyc; sh run_exp.sh $RUNTIME) ; (cd tinyc/eval/; sh comp_run.sh tiny) ) &
+( (cd mjs; sh run_exp.sh $RUNTIME) ; (cd mjs/eval/; sh comp_run.sh mjs) ) &
 
 #/bin/sleep ${RUNTIME}h
 
-#some safety buffer s.t. all is properly closed and everything had enough time to stop
-#/bin/sleep 15m
-wait
-
-(cd csv/eval/; sh comp_run.sh csvparser) &
-(cd ini/eval/; sh comp_run.sh ini) &
-(cd cjson/eval/; sh comp_run.sh cJSON) &
-(cd tinyc/eval/; sh comp_run.sh tiny) &
-(cd mjs/eval/; sh comp_run.sh mjs) &
 wait
